ae011305e139d0adc01a1acf848f54b4f5ed772d
pistachio <pistaio@protonmail.com>
2022-03-20 17:36:01 +0530

    improvements

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# lighthouse
+# Lighthouse
+
 An implementation of a Bitcoin Lightning Network node. Just a disclaimer, I am new to Lightning and new to Rust. So expect progress on this project to be slow and riddled with bugs. I see this as a way for me to learn both and hopefully help for us learn together. Feel free to get in touch so that we can work on this together.
 
 ## Some tools to help development
@@ -8,17 +9,21 @@ An implementation of a Bitcoin Lightning Network node. Just a disclaimer, I am n
 
 ## My current understanding
 
-Lightning is a P2P network built on top of the Bitcoin blockchain. A transaction is 'sent' to LN which 'locks' the bitcoin in the Lightning ecosystem. By 'sent' I mean a UTXO is sent to a multisig address generated during the opening of a channel between 2 peers. Bitcoin gets 'locked' (or rather freed) because it cannot be spent on-chain anymore until a closing transaction is initiated from the LN. 
+Lightning is a P2P network built on top of the Bitcoin blockchain. A transaction is 'sent' to LN which 'locks' the bitcoin in the Lightning ecosystem. By 'sent' I mean a UTXO is sent to a multisig address generated during the opening of a channel between 2 peers. Bitcoin gets 'locked' (or rather freed) because it cannot be spent on-chain anymore until a closing transaction is initiated from the LN. It gets freed to be moved around in the LN, which is off-chain. For the most part, LN is more private because only the channels involved in the payment know about the transaction, whereas in Bitcoin each transaction is broadcasted on the blockchain.
 
-Step 1 in the creation of the node will be to connect to bitcoin core (eg. `bitcoind`). 
-
-Step 2 will be to connect to a peer node.
-
-Step 3 will be to open a channel between the two nodes. This will entail creating a funding transaction.
+- Step 1 in the creation of the node will be to connect to bitcoin core (eg. `bitcoind`). 
+- Step 2 will be to connect to a peer node.
+    - This is estabilishing an internet connection with the node so that a channel can be opened
+    - Gossip node public key and network address to announce node as public (v2)
+- Step 3 will be to open a channel between the 2 nodes.
+    - Create a 2-of-2 multisig address
+    - Create presigned transaction that spends the multisig output to prevent loss of funds
+    - Channel open initiator transfers funds to this address (called the funding transaction)
 
 
 ## Theory
 
 - bitcoind (Bitcoin client)
     - [https://en.bitcoin.it/wiki/Clients](https://en.bitcoin.it/wiki/Clients)
+    - [Bitcoin Core: The Reference Implementation](https://github.com/bitcoinbook/bitcoinbook/blob/develop/ch03.asciidoc)
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use bitcoin::{BlockHash, hashes::hex::FromHex};
+use lightning_block_sync::{http::{HttpEndpoint, JsonResponse}, rpc::RpcClient};
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
@@ -5,46 +8,56 @@ async fn main() {
     let password = "polarpass".to_string();
     let rpc_host= "127.0.0.1".to_string();
     let port: u16 = 18443;
+    let network_chain = "regtest".to_string();
 
-    bitcoind_client::connect_to_bitcoin_core(username, password, rpc_host, port).await;
+    let bitcoind_client = BitcoinClient::connect_to_bitcoin_node(username, password, rpc_host, port);
+    assert_eq!(bitcoind_client.getblockchaininfo().await.chain, network_chain);
+    println!("i guess it worked? i didn't specify the chain anywhere tho");
 }
 
-mod bitcoind_client {
-    use bitcoin::{BlockHash, hashes::hex::FromHex};
-    use lightning_block_sync::{http::{HttpEndpoint, JsonResponse}, rpc::RpcClient};
+struct BitcoinClient {
+    client: RpcClient
+}
 
-    #[derive(Debug)]
-    pub struct BlockchainInfo {
-        pub latest_height: usize,
-        pub latest_blockhash: BlockHash,
-        pub chain: String,
-    }
-
-    impl TryInto<BlockchainInfo> for JsonResponse {
-        type Error = std::io::Error;
-        fn try_into(self) -> std::io::Result<BlockchainInfo> {
-            Ok(BlockchainInfo {
-                latest_height: self.0["blocks"].as_u64().unwrap() as usize,
-                latest_blockhash: BlockHash::from_hex(self.0["bestblockhash"].as_str().unwrap())
-                    .unwrap(),
-                chain: self.0["chain"].as_str().unwrap().to_string(),
-            })
-        }
-    }
-
-    pub async fn connect_to_bitcoin_core(username: String, password: String,
-            rpc_host: String, port: u16) {
-
-        let http_endpoint = HttpEndpoint::for_host(rpc_host).with_port(port);
+impl BitcoinClient {
+    pub fn connect_to_bitcoin_node(username: String, password: String,
+            rpc_host: String, port: u16) -> BitcoinClient {
+        let http_endpoint = HttpEndpoint::for_host(rpc_host.clone()).with_port(port);
         let credentials_string = format!("{}:{}", username, password);
         let credentials = base64::encode(credentials_string);
-        let mut bitcoind_rpc_client = RpcClient::new(&credentials, http_endpoint).unwrap();
+        let bitcoind_rpc_client = RpcClient::new(&credentials, http_endpoint).unwrap();
+        let client = BitcoinClient {
+            client: bitcoind_rpc_client
+        };
+        return client 
+}
 
-        let info = bitcoind_rpc_client.call_method::<BlockchainInfo>("getblockchaininfo", &[]).await.map_err(|_| {
-				std::io::Error::new(std::io::ErrorKind::PermissionDenied,
-				"Failed to make initial call to bitcoind - please check your RPC user/password and access settings")
-			});
-        println!("block info: {:#?}", info);
+    pub async fn getblockchaininfo(mut self) -> BlockchainInfo {
+        let info = self.client.call_method::<BlockchainInfo>("getblockchaininfo", &[]).await.map_err(|_| {
+            std::io::Error::new(std::io::ErrorKind::PermissionDenied,
+                    "Failed to make initial call to bitcoind - please check your RPC user/password and access settings")
+            }).expect("Some error");
+        // println!("block info: {:#?}", info);
+        return info
+    }
+}
+
+#[derive(Debug)]
+pub struct BlockchainInfo {
+    pub latest_height: usize,
+    pub latest_blockhash: BlockHash,
+    pub chain: String,
+}
+
+impl TryInto<BlockchainInfo> for JsonResponse {
+    type Error = std::io::Error;
+    fn try_into(self) -> std::io::Result<BlockchainInfo> {
+        Ok(BlockchainInfo {
+            latest_height: self.0["blocks"].as_u64().unwrap() as usize,
+            latest_blockhash: BlockHash::from_hex(self.0["bestblockhash"].as_str().unwrap())
+                .unwrap(),
+            chain: self.0["chain"].as_str().unwrap().to_string(),
+        })
     }
 }
 
