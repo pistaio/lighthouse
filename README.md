2f8b7474611d8b72d69317ee64836cdc6c522a89
pistachio <pistaio@protonmail.com>
2022-03-20 16:02:25 +0530

    Hardcode connect to bitcoind client

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # lighthouse
 An implementation of a Bitcoin Lightning Network node. Just a disclaimer, I am new to Lightning and new to Rust. So expect progress on this project to be slow and riddled with bugs. I see this as a way for me to learn both and hopefully help for us learn together. Feel free to get in touch so that we can work on this together.
 
+## Some tools to help development
+
+- Polar
+- Lightning Development Kit (LDK)
+
 ## My current understanding
 
 Lightning is a P2P network built on top of the Bitcoin blockchain. A transaction is 'sent' to LN which 'locks' the bitcoin in the Lightning ecosystem. By 'sent' I mean a UTXO is sent to a multisig address generated during the opening of a channel between 2 peers. Bitcoin gets 'locked' (or rather freed) because it cannot be spent on-chain anymore until a closing transaction is initiated from the LN. 
@@ -12,7 +17,8 @@ Step 2 will be to connect to a peer node.
 Step 3 will be to open a channel between the two nodes. This will entail creating a funding transaction.
 
 
-## Some tools to help development
+## Theory
 
-- Polar
-- Lightning Development Kit (LDK)
+- bitcoind (Bitcoin client)
+    - [https://en.bitcoin.it/wiki/Clients](https://en.bitcoin.it/wiki/Clients)
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let username = "polaruser".to_string();
+    let password = "polarpass".to_string();
+    let rpc_host= "127.0.0.1".to_string();
+    let port: u16 = 18443;
+
+    bitcoind_client::connect_to_bitcoin_core(username, password, rpc_host, port).await;
 }
+
+mod bitcoind_client {
+    use bitcoin::{BlockHash, hashes::hex::FromHex};
+    use lightning_block_sync::{http::{HttpEndpoint, JsonResponse}, rpc::RpcClient};
+
+    #[derive(Debug)]
+    pub struct BlockchainInfo {
+        pub latest_height: usize,
+        pub latest_blockhash: BlockHash,
+        pub chain: String,
+    }
+
+    impl TryInto<BlockchainInfo> for JsonResponse {
+        type Error = std::io::Error;
+        fn try_into(self) -> std::io::Result<BlockchainInfo> {
+            Ok(BlockchainInfo {
+                latest_height: self.0["blocks"].as_u64().unwrap() as usize,
+                latest_blockhash: BlockHash::from_hex(self.0["bestblockhash"].as_str().unwrap())
+                    .unwrap(),
+                chain: self.0["chain"].as_str().unwrap().to_string(),
+            })
+        }
+    }
+
+    pub async fn connect_to_bitcoin_core(username: String, password: String,
+            rpc_host: String, port: u16) {
+
+        let http_endpoint = HttpEndpoint::for_host(rpc_host).with_port(port);
+        let credentials_string = format!("{}:{}", username, password);
+        let credentials = base64::encode(credentials_string);
+        let mut bitcoind_rpc_client = RpcClient::new(&credentials, http_endpoint).unwrap();
+
+        let info = bitcoind_rpc_client.call_method::<BlockchainInfo>("getblockchaininfo", &[]).await.map_err(|_| {
+				std::io::Error::new(std::io::ErrorKind::PermissionDenied,
+				"Failed to make initial call to bitcoind - please check your RPC user/password and access settings")
+			});
+        println!("block info: {:#?}", info);
+    }
+}
+
